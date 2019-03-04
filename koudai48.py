72ffb6cc6937489da18f1451ee985ba037370a4d
loony175 <l1997y03w01@gmail.com>
2019-03-05 02:22:59 +0800

    Updated at 2019-03-05T02:22:59Z (UTC+8)

--- koudai48.py
+++ koudai48.py
@@ -12,6 +12,7 @@ import os
 import pathlib
 import re
 import requests
+from urllib import parse
 
 def process(dict):
     info={}
@@ -21,13 +22,19 @@ def process(dict):
     sub_title['base64']=base64.b64encode(dict['subTitle'].encode()).decode()
     info['subTitle']=sub_title
     info['picPath']=['https://source.48.cn%s'%obj for obj in dict['picPath'].split(',')]
+    datetime=arrow.get(dict['startTime']/1000).to('Asia/Shanghai')
     start_time={}
     start_time['timestamp']=dict['startTime']
-    start_time['datetime']=arrow.get(dict['startTime']/1000).to('Asia/Shanghai').strftime('%Y-%m-%dT%H:%M:%SZ')
+    start_time['datetime']=datetime.strftime('%Y-%m-%dT%H:%M:%SZ')
     info['startTime']=start_time
     info['memberId']=dict['memberId']
     info['liveType']=dict['liveType']
     info['streamPath']=dict['streamPath'].replace('http://','https://')
+    if parse.urlparse(info['streamPath']).hostname=='alcdn.hls.xiaoka.tv':
+        date=re.match(r'^.*/(\d{6,8})/.*$',info['streamPath']).group(1)
+        date_=datetime.format('YYYYMD')
+        if date_!=date:
+            info['streamPath']=info['streamPath'].replace(date,date_)
     return info
 
 def dump(data,json_data,member):
